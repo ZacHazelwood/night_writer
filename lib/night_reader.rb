0aa6c62f4f24719a062ef2542aba249ade9b14ff
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-21 18:09:34 -0600

    Create NightWriter initialize

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,6 +2,10 @@ require "./lib/file_reader"
 
 class NightReader
 
+  attr_reader :file_reader
 
+  def initialize
+    @file_reader = FileReader.new.read
+  end
 
 end
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -6,9 +6,12 @@ require './lib/night_reader'
 
 RSpec.describe NightReader do
 
-  it "exists" do
+  it "exists and can read from FileReader" do
     night_reader = NightReader.new
+    text = "hello world"
+    night_reader.stub(:read).and_return(text)
 
     expect(night_reader).to be_a(NightReader)
+    expect(night_reader.read).to eq(text)
   end
 end
