78f3ae357f29787a3177f40046e6392d3a00397a
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-23 14:50:24 -0600

    Create english-to-braille alphabet; Test hash

--- spec/english_to_braille_spec.rb
+++ spec/english_to_braille_spec.rb
@@ -4,11 +4,20 @@ SimpleCov.start
 require './lib/english_to_braille'
 
 RSpec.describe EnglishToBraille do
+  before :each do
+    @dictionary = EnglishToBraille.new
+  end
 
   it "exists" do
-    dictionary = EnglishToBraille.new
 
-    expect(dictionary).to be_a(EnglishToBraille)
+    expect(@dictionary).to be_a(EnglishToBraille)
+  end
+
+  it "contains a hash of english letters to braille" do
+
+    expect(@dictionary.english_to_braille_hash).to be_a(Hash)
+    expect(@dictionary.english_to_braille_hash["a"]).to eq(["0.", "..", ".."])
+    expect(@dictionary.english_to_braille_hash[" "]).to eq(["..", "..", ".."])
   end
 
 end
