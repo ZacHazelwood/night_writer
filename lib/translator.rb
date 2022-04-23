04bfafbaec80213d173870f9a48f474d008a2c14
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-23 17:55:53 -0600

    Refactor for testing; Add #convert_to_braille method

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,26 @@
+require './lib/english_to_braille'
+
 class Translator
+  include EnglishToBraille
 
   attr_reader :text,
               :dictionary
 
-  def initialize(text, dictionary)
-    @text = text
-    @dictionary = dictionary
+  # def initialize(text, dictionary)
+  #   @text = text
+  #   @dictionary = dictionary
+  # end
+
+  def initialize(test)
+    @test = test
+    @converted_braille = []
+  end
+
+  def convert_to_braille
+    english_letters = @test.chars
+    (english_letters.count).times do |letter|
+      @converted_braille << english_to_braille_hash[english_letters.shift]
+    end
+      @converted_braille
   end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -5,19 +5,39 @@ require './lib/translator'
 require './lib/english_to_braille'
 
 RSpec.describe Translator do
-  before :each do
-    @dictionary = EnglishToBraille.new
-    @translator = Translator.new("a", @dictionary)
-  end
-  it "exists" do
+  # before :each do
+  #   @dictionary = EnglishToBraille.new
+  #   @translator = Translator.new("a", @dictionary)
+  # end
+  xit "exists" do
 
     expect(@translator).to be_a(Translator)
   end
 
-  it "has attributes" do
+  xit "has attributes" do
 
     expect(@translator.text).to eq("a")
     expect(@translator.dictionary).to be_a(EnglishToBraille)
+  end
+
+# Refactor tests for testing
+
+  it "can convert 1 letter to braille" do
+    translator = Translator.new("a")
+
+    expect(translator.convert_to_braille).to eq([["0.", "..", ".."]])
+  end
+
+  it "can convert 2 letters to braille" do
+    translator = Translator.new("az")
+
+    expect(translator.convert_to_braille).to eq([["0.", "..", ".."], ["0.", ".0", "00"]])
+  end
+
+  it "can check for spaces" do
+    translator = Translator.new("a z")
+
+    expect(translator.convert_to_braille).to eq([["0.", "..", ".."], ["..", "..", ".."], ["0.", ".0", "00"]])
   end
 
 end
