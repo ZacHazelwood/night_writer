1f32010eb78ef4c9cdfe6830726c78a87e775868
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-24 18:42:29 -0600

    Test line breaks after 40 characters

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -6,14 +6,14 @@ require './lib/english_to_braille'
 
 RSpec.describe Translator do
 
-  it "exists" do
+  xit "exists" do
     text = "a"
     translator = Translator.new(text)
 
     expect(translator).to be_a(Translator)
   end
 
-  it "has attributes" do
+  xit "has attributes" do
     text = "a"
     translator = Translator.new(text)
 
@@ -21,28 +21,28 @@ RSpec.describe Translator do
     expect(translator.dictionary).to be_a(Hash)
   end
 
-  it "can convert 1 letter to braille" do
+  xit "can convert 1 letter to braille" do
     text = "a"
     translator = Translator.new(text)
 
     expect(translator.convert_to_braille).to eq([["0.", "..", ".."]])
   end
 
-  it "can convert 2 letters to braille" do
+  xit "can convert 2 letters to braille" do
     text = "az"
     translator = Translator.new(text)
 
     expect(translator.convert_to_braille).to eq([["0.", "..", ".."], ["0.", ".0", "00"]])
   end
 
-  it "can check for spaces" do
+  xit "can check for spaces" do
     text = "a z"
     translator = Translator.new(text)
 
     expect(translator.convert_to_braille).to eq([["0.", "..", ".."], ["..", "..", ".."], ["0.", ".0", "00"]])
   end
 
-  it "can arrange 1 braille letter vertically" do
+  xit "can arrange 1 braille letter vertically" do
     text = "a"
     translator = Translator.new(text)
 
@@ -51,11 +51,20 @@ RSpec.describe Translator do
     expect(translator.arrange_braille).to eq(expected)
   end
 
-  it "can arrange multiple braille letters vertially" do
+  xit "can arrange multiple braille letters vertially" do
     text = "aaaa"
     translator = Translator.new(text)
 
-    expected = "0.0.0.0.\n........\n........"
+    expected = "0.0.0.0.\n........\n......"
+
+    expect(translator.arrange_braille).to eq(expected)
+  end
+
+  it "can add a line break after 40 characters" do
+    text = "abcdefghi abcdefghi abcdefghi abcdefghi a"
+    translator = Translator.new(text)
+
+    expected = ""
 
     expect(translator.arrange_braille).to eq(expected)
   end
