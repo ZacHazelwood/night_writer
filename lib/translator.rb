86996b90e8baf40d571061c27d1dd888640c0dd7
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-25 15:40:47 -0600

    Clean code; Add comments

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,15 +11,8 @@ class Translator
     @dictionary = ENGLISH_TO_BRAILLE_HASH
   end
 
-  def convert_to_braille
-    english_letters = @text.chars
-    converted_braille = []
-        (english_letters.count).times do |letter|
-          converted_braille << @dictionary[english_letters.shift]
-        end
-    converted_braille.each_slice(40).to_a
-  end
-
+# Iterates through the "joined_sentences" variable and add them to a string,
+# with each line separated by a line-break: "\n".
   def arrange_braille
     output_braille = ""
       join_sentences.each do |line|
@@ -28,6 +21,19 @@ class Translator
     output_braille
   end
 
+# Helper methods for #arrange_braille
+
+# Step 1: Convert English text into Braille
+  def convert_to_braille
+    english_letters = @text.chars
+    converted_braille = []
+    (english_letters.count).times do |letter|
+      converted_braille << @dictionary[english_letters.shift]
+    end
+    converted_braille.each_slice(40).to_a
+  end
+
+# Step 2: Transpose the converted braille into separate Array elements
   def transpose_conversion
     transposed_sentence = []
     translated_braille = self.convert_to_braille
@@ -37,6 +43,8 @@ class Translator
     transposed_sentence
   end
 
+# Step 3: Combine each separate top, middle, bottom of the Braille characters
+# into "sentences." Each Array element contains a full line of Braille
   def format_into_sentences
     format_sentences = []
     transpose_conversion.map do |segment|
@@ -48,6 +56,8 @@ class Translator
     format_sentences
   end
 
+# Step 4: Iterates through the "sentences" to combine each line of text into 1
+# unified Array.
   def join_sentences
     joined_sentences = []
     format_into_sentences.each do |sentence|
@@ -57,29 +67,4 @@ class Translator
     end
     joined_sentences
   end
-  # def arrange_braille
-  #   transposed_sentence = []
-  #   format_sentences = []
-  #   joined_sentences = []
-  #   output_braille = ""
-  #   translated_braille = self.convert_to_braille
-  #     translated_braille.each do |sentence|
-  #       transposed_sentence << sentence.transpose
-  #     end
-  #     transposed_sentence.map do |segment|
-  #       top = segment[0].join
-  #       mid = segment[1].join
-  #       bot = segment[2].join
-  #       format_sentences << [top, mid, bot]
-  #     end
-  #     format_sentences.each do |sentence|
-  #       sentence.each do |line|
-  #         joined_sentences << line
-  #       end
-  #     end
-  #     joined_sentences.each do |line|
-  #       output_braille += "#{line}\n"
-  #     end
-  #   output_braille
-  # end
 end
