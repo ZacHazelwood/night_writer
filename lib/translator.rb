5afc261a7752565133bed05184b3390b354d8615
Zachary Hazelwood <zhaze001@gmail.com>
2022-04-26 13:18:19 -0600

    Remove comments

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,6 @@ class Translator
     @dictionary = ENGLISH_TO_BRAILLE_HASH
   end
 
-# Iterates through the "joined_sentences" variable and add them to a string,
-# with each line separated by a line-break: "\n".
   def arrange_braille
     output_braille = ""
       join_sentences.each do |line|
@@ -23,7 +21,6 @@ class Translator
 
 # Helper methods for #arrange_braille
 
-# Step 1: Convert English text into Braille
   def convert_to_braille
     english_letters = @text.chars
     converted_braille = []
@@ -33,7 +30,6 @@ class Translator
     converted_braille.each_slice(40).to_a
   end
 
-# Step 2: Transpose the converted braille into separate Array elements
   def transpose_conversion
     transposed_sentence = []
     translated_braille = self.convert_to_braille
@@ -43,8 +39,6 @@ class Translator
     transposed_sentence
   end
 
-# Step 3: Combine each separate top, middle, bottom of the Braille characters
-# into "sentences." Each Array element contains a full line of Braille
   def format_into_sentences
     format_sentences = []
     transpose_conversion.map do |segment|
@@ -56,8 +50,6 @@ class Translator
     format_sentences
   end
 
-# Step 4: Iterates through the "sentences" to combine each line of text into 1
-# unified Array.
   def join_sentences
     joined_sentences = []
     format_into_sentences.each do |sentence|
